854beb84cccba840cf295d16519e44a40a50f597
Yuankaifeng <yuankaifeng@email.com>
2026-05-27 10:44:10 +0800

    Ready for Vercel deployment

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,12 @@ import Vue from 'vue';
 import Router from 'vue-router';
 Vue.use(Router);
 export default new Router({
-    mode: 'hash',
+    mode: 'history',
     routes: [
         {
             path: '/',
             component: () => import( /* webpackChunkName: "main" */ '@/views/index/index.vue'),
             meta: { title: '首页' },
-            redirect: '/index',
             children: []
         },
         {
